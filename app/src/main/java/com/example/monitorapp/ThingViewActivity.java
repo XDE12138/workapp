0a5f5bcbe38dfc4509c8d04357f47e30d5cee4bd
XDE <50771219+xde12138@users.noreply.github.com>
2020-08-16 05:06:19 +0800

    改进了设备界面，加入了卡片界面

--- app/src/main/java/com/example/monitorapp/ThingViewActivity.java
+++ app/src/main/java/com/example/monitorapp/ThingViewActivity.java
@@ -1,14 +1,48 @@
 package com.example.monitorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Future;
+
 public class ThingViewActivity extends AppCompatActivity {
+
+    private Fruit[] fruits = {new Fruit("正常", R.drawable.kuang1),
+            new Fruit("故障", R.drawable.kuang2),
+            new Fruit("正常", R.drawable.kuang5),
+            new Fruit("正常", R.drawable.kuang7),
+            new Fruit("正常", R.drawable.kuang8),
+            new Fruit("未工作", R.drawable.kuang9)};
+
+    private List<Fruit> fruitList = new ArrayList<>();
+
+    private FruitAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thing_view);
+
+        initFruits();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new FruitAdapter(fruitList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initFruits() {
+        fruitList.clear();
+        for (int i = 0; i < 50; i++) {
+            Random random = new Random();
+            int index = random.nextInt(fruits.length);
+            fruitList.add(fruits[index]);
+        }
     }
 }
